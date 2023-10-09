21db794da96817cb7a88eb81056d1e2f8cda6965
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-10-09 15:09:34 +0530

    added primitive types

---
+++ syntax-js/02-primitive_types.js
@@ -0,0 +1,30 @@
+console.log("Primitive types in js")
+
+// null type
+let a = null;
+console.log(a, " ", typeof a)
+
+// number type
+let b = 345;
+console.log(b, " ", typeof b)
+
+// boolean type
+let c = true; // can also be false
+console.log(c, " ", typeof c)
+
+// biginteger type
+let d = BigInt("567") + BigInt("3")
+console.log(d, " ", typeof d)
+
+// string type
+let e = "javascript"
+console.log(e, " ", typeof e)
+
+// symbol type
+let f = Symbol("I am a nice symbol")
+console.log(f, " ", typeof f)
+
+// undefined type
+let g
+console.log(g, " ", typeof g)
+
