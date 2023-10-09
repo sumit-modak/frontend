c8b64243211b5cbb562eeb1df03960adf127ddff
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-10-09 15:06:47 +0530

    added declaring variables

---
+++ syntax-js/01-declaring_variables.js
@@ -0,0 +1,23 @@
+console.log("JavaScript tutorial for declaring variables")
+
+// declaring variable normally
+let a = "javascript";
+console.log(a);
+
+// declaring constants (immutable variables)
+const pi = 3.14;
+console.log(pi);
+
+// declaring global variable
+var b = 123211003169;
+console.log(b);
+
+let c = null
+let d  = undefined  
+
+{
+  let x = 'this'
+  console.log(x)
+}
+// error cuz x is deallocated when it got out of scope
+console.log(x) 
