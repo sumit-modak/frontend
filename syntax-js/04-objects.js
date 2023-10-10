58954a63eb79025a02236a05e9a7c8b228275d6b
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-10-10 09:24:46 +0530

    added objects

---
+++ syntax-js/04-objects.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "Sumit",
+  section: "C",
+  group: 1,
+}
+console.log(typeof student)
+console.log(student)
+
+student['roll'] = "123211003169"  
+student['id'] = "JIS/2021/0441"
+console.log(student)
+console.log(student['id'])
