3cd56b3ec36f0b40ae96b03d4b8acd77c0923f4b
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-10-10 23:16:28 +0530

    added switch case

---
+++ syntax-js/06-switch_case.js
@@ -0,0 +1,20 @@
+let x = prompt("Enter val: ");
+
+switch (x) {
+  case "O":
+    console.log("You're outstanding"); break;
+  case "E":
+    console.log("You're excellent"); break;
+  case "A":
+    console.log("You're good"); break;
+  case "B":
+    console.log("You're average"); break;
+  case "C":
+    console.log("You're low"); break;
+  case "D":
+    console.log("You're somehow passed"); break;
+  case "F":
+    console.log("You have failed"); break;
+  default:
+    console.log("Enter correctly bro");
+}
